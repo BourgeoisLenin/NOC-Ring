726dde2ac240eb340716d3a5b748ab681fe03780
BourgeoisLenin <43970977+BourgeoisLenin@users.noreply.github.com>
2023-03-23 19:47:10 -0700

    Add files via upload

--- cpu/ID.v
+++ cpu/ID.v
@@ -22,7 +22,7 @@ module ID (
     input wire EX_MEM_wrEn;
 
     output wire [0:63] ID_rA_data, ID_rB_data;
-    output wire [0:4] ID_rD,ID_rA,ID_rB;
+    output wire [0:4] ID_rD,ID_rA,ID_rB; //may not need ra rb to be output
     output wire [0:1] ID_WW;
     output wire [0:2] ID_ppp;
     output wire ID_wrEn,ID_memEn,ID_memwrEn;
@@ -35,7 +35,7 @@ module ID (
     wire ID_R_type;
     wire [0:4] rf_in2;
 
-    assign rf_in2 = ID_R_type? ID_rB: ID_rD;
+    assign rB_or_rD = ID_R_type? ID_rB: ID_rD;
 
     decode_ctrl decode_ctrl(ID_inst,
     ID_wrEn,ID_rD,ID_rA,ID_rB,ID_WW, ID_ppp,
@@ -44,9 +44,9 @@ module ID (
     //connect M_type_rD_data to ID_rB_data - done
     br_ctrl br_ctrl(ID_decode_ctrl_bez,ID_decode_ctrl_bnez, ID_rB_data,ID_br_ctrl);
 
-    RF reg_file(clk,reset,WB_wrEn,ID_rA,rf_in2,WB_rD,WB_ppp,WB_rD_data,ID_rA_data,ID_rB_data);
+    RF reg_file(clk,reset,WB_wrEn,ID_rA,rB_or_rD,WB_rD,WB_ppp,WB_rD_data,ID_rA_data,ID_rB_data);
 
-    forwarding_unit forward_ctrl(ID_rA, ID_rB, EX_MEM_rD, EX_MEM_wrEn, ID_forward_rA, ID_forawrd_rB);
+    forwarding_unit forward_ctrl(ID_rA, rB_or_rD, EX_MEM_rD, EX_MEM_wrEn, ID_forward_rA, ID_forawrd_rB);
 
     //branch and hdu
 
